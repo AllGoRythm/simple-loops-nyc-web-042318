dfc78d8ca74f4e4a7668322f7f17520b5b798fa0
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-11 20:22:57 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,7 +13,7 @@ def loop_iterator(number_of_times)
 counter = 0
   loop do
     counter += 1
-    return "Welcome to Flatiron School's Web Development Course!"
+    puts "Welcome to Flatiron School's Web Development Course!"
   if counter < 7
     break
   end
